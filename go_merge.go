7b607546d7338faa7c42a89523039e2cb8e058fd
Oleh Kolomiitsev <kolomiytsev.oleg@gmail.com>
2020-02-25 22:55:39 +0200

    Merge sorted array.

--- go_merge.go
+++ go_merge.go
@@ -1,25 +1,32 @@
 package main
 
-import ("log")
+import "log"
 
 func main() {
 	arrA := [5]int{0, 1, 2, 4, 100}
-	arrB := [4]int{5, 6, 7, 8}
+	log.Println("arrA", arrA)
+	arrB := [6]int{5, 6, 7, 8, -1, 8}
+	log.Println("arrB", arrB)
 
-	arrC := make([]int, len(arrA), len(arrA) + len(arrB))
+	arrC := make([]int, len(arrA))
 	copy(arrC, arrA[:])
-	log.Println(arrC)
-	arrC = append(arrC[:1], 99)
-	log.Println(arrC)
-
-	// for i := 0; i < len(arrB); i++ {
-	// 	toInsert := arrB[i]
-	// 	for j := 0; j < len(arrC); j++ {
-	// 		if toInsert > arrC[j] {
-	// 			arrC = append(arrC[:j], toInsert)
-	// 			break
-	// 		}
-	// 	}
-	// }
-	// log.Println(arrC)
+	log.Println("arrC", arrC)
+	for i := 0; i < len(arrB); i++ {
+		toInsert := arrB[i]
+		for j := 0; j < len(arrC); j++ {
+			value := arrC[j]
+			if toInsert == value {
+				log.Println("Skip:", toInsert)
+				break
+			}
+			if toInsert < value {
+				log.Println("toInsert:", toInsert)
+				arrC = append(arrC, 0)
+				copy(arrC[j+1:], arrC[j:])
+				arrC[j] = toInsert
+				break
+			}
+		}
+	}
+	log.Println("merged:", arrC, "len:", len(arrC))
 }
